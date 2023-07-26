1b6fe8c3173113a96d6dbb749bb502b70657691d
Koen Hufkens <koen.hufkens@gmail.com>
2023-07-26 15:15:14 +0200

    render training dataset

--- data-raw/02_compose_machine_learning_data.R
+++ data-raw/02_compose_machine_learning_data.R
@@ -6,6 +6,12 @@
 # load libraries
 library(tidyverse)
 
+# read in raw data (long format)
+site_data <- readRDS("data/MODIS_reflectance_data.rds")
+
+# read in flue data
+flue <- readr::read_csv("data/flue_stocker18nphyt.csv")
+
 # pivot wider for easier band math
 site_data_wide <- site_data |>
   select(
@@ -29,17 +35,33 @@ site_data_wide <- site_data_wide |>
     pri = (sur_refl_b11  - sur_refl_b12 )/(sur_refl_b11  + sur_refl_b12 )
   )
 
+# merge with flue
+site_data_wide <- left_join(
+  flue,
+  site_data_wide,
+  by = c("site", "date")
+  )
+
 # interpolate missing values and daily
 # data for the full time series
 
+# 1. create long term mean
+# 2. use long term mean weights in spline based gap filling
+# (see phenocam routine)
 
+# for now do a quick linear interpolation
+site_data_wide <- site_data_wide |>
+  mutate(
+    across(
+      where(is.double),
+      \(x) zoo::na.approx(x, na.rm = FALSE)
+      )
+  )
 
-
-
-
-# save the data with the indices (as a wide format)
-# saveRDS(
-#   site_data,
-#   "data/MODIS_reflectance_data_indices.rds",
-#   compress = "xz"
-# )
+# save the data with the indices and flue values
+# for machine learning training
+saveRDS(
+  site_data,
+  "data/machine_learning_training_data.rds",
+  compress = "xz"
+)
