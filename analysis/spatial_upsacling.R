7ec154e633aa92066181ec3e2d4496f037f66985
Koen Hufkens <koen.hufkens@gmail.com>
2023-12-04 12:11:40 +0100

    spatial upscaling routine

--- analysis/spatial_upsacling.R
+++ analysis/spatial_upsacling.R
@@ -6,46 +6,74 @@ library(tidymodels)
 library(ranger)
 library(dplyr)
 library(terra)
+library(ggplot2)
+library(rnaturalearth)
+library(tidyterra)
 set.seed(0)
+
+# read in precompiled model
+regression_model <- readRDS(
+  here::here("data/regression_model.rds")
+)
 
 files <- data.frame(
   file = list.files("data-raw/modis_data_spatial/","*.tif", full.names = TRUE)
 )
 
-files <- files |>
-  dplyr::filter(
-    grepl("doy2018210",file)
+doys <- 180:300
+
+lapply(doys, function(doy){
+
+  files <- files |>
+    dplyr::filter(
+      grepl(sprintf("doy2018%s",as.character(doy)),file)
+    )
+
+  r <- terra::rast(files$file)
+
+  # the model only works when variable names
+  # are consistent we therefore rename them
+  band_names <- data.frame(
+    name = names(r)
+  ) |>
+    mutate(
+      date = as.Date(substr(name, 40, 46), format = "%Y%j"),
+      name = substr(name, 13, 35)
+    )
+
+  # reassign the names of the terra image stack
+  names(r) <- band_names$name
+
+  # return probabilities, where each class is
+  # associated with a layer in an image stack
+  # and the probabilities reflect the probabilities
+  # of the classification for said layer
+  p <- terra::predict(
+    r,
+    regression_model
   )
 
-r <- terra::rast(files$file)
+  # grab country polygons from world map
+  # restrict to selected country
+  country <- ne_countries(
+    scale = 50,
+    returnclass = "sf"
+  ) |>
+    dplyr::filter(
+      sovereignt %in%  c("Switzerland","Germany","Austria")
+    ) |>
+    sf::st_union() |>
+    sf::st_as_sf()
 
+  p <- ggplot() +
+    geom_spatraster(
+      data = p
+    ) +
+    scale_fill_viridis_c() +
+    geom_sf(data = country, colour = "white", fill = NA) +
+    labs(
+      title = doy
+    )
 
-# the model only works when variable names
-# are consistent we therefore rename them
-band_names <- data.frame(
-  name = names(r)
-) |>
-  mutate(
-    date = as.Date(substr(name, 40, 46), format = "%Y%j"),
-    name = substr(name, 13, 35)
-  )
-
-# reassign the names of the terra image stack
-names(r) <- band_names$name
-
-# read in precompiled model
-classification_model <- readRDS(
-  here::here("data/classification_model.rds")
-)
-
-# return probabilities, where each class is
-# associated with a layer in an image stack
-# and the probabilities reflect the probabilities
-# of the classification for said layer
-probabilities <- terra::predict(
-  r,
-  classification_model,
-  type = "prob"
-)
-
-plot(probabilities$.pred_TRUE > 0.8)
+  ggsave(filename = sprintf("manuscript/%s.png", as.character(doy)), p, width = 5, height = 5)
+})
