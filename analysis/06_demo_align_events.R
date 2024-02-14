c90e0a5f124da467ed83a0da3d81eb927bb7d37a
Koen Hufkens <koen.hufkens@gmail.com>
2024-02-14 21:19:00 +0100

    align events

--- analysis/06_demo_align_events.R
+++ analysis/06_demo_align_events.R
@@ -1,71 +1,86 @@
 library(readr)
 library(ggplot2)
 library(dplyr)
-source("R/align_events.R")
-
-# use flue data for demo
-flue <- readr::read_csv("data/flue_stocker18nphyt.csv") |>
-  dplyr::filter(site == "FR-Pue")
-
-# this data has column is_flue_drought which is a binary classification
-# of whether the respective date is a drought or not.
-# Drought is defined here by fLUE being below 1 (with a certain threshold, see
-# Stocker et al., 2018).
-
-# determine events as consecutive time periods of dates classified as 'flue drought'
-# this returns the event start and length index of the time series of the binary
-# classification, here is_flue_drought
-df_idx_events <- get_consecutive(
-  dry = flue$is_flue_drought,
-  leng_threshold = 5,
-  do_merge = FALSE
-  ) %>%
-  mutate(
-    date_start = flue$date[.$idx_start],
-    date_end = flue$date[.$idx_start + .$len - 1]
-    )
-
-# plot flue and events
-ggplot() +
-  geom_line(
-    data = flue |>
-      filter(site == "FR-Pue"),
-    aes(date, flue),
-    color = "tomato"
-  ) +
-  geom_rect(
-    data = df_idx_events,
-    aes(xmin = date_start, xmax = date_end, ymin = 0, ymax = 1.2),
-    color = NA,
-    fill = "black",
-    alpha = 0.3
-  ) +
-  theme_classic() +
-  ylim(0,1.2)
+library(tidyr)
 
 
-# align data by event (function get_consecutive is called again inside align_events() - not nice)
-flue_aligned <- align_events(
-  df = flue |>
-    rename(
-      isevent = is_flue_drought
-      ),
-  df_isevent = NA,
-  dovars = "flue",
-  leng_threshold = 5, # require at least five consecutive days to create an 'event'
-  before = 5,
-  after = 150,
-  nbins = 10 # for data normalisation
-)
+df <- readRDS(here::here("data/machine_learning_training_data.rds"))
+#
+# df <- df |>
+#   dplyr::mutate(
+#     N = Nadir_Reflectance_Band2,
+#     S1 = Nadir_Reflectance_Band6,
+#     S2 = Nadir_Reflectance_Band7,
+#     swir_diff = S1 - S2,
+#     NBR = (S1-S2)/(S1+S2),
+#     NMDI = (N-(S1-S2))/(N+(S1-S2)), # normalized multiband drought index
+#     GVMI = ((N+0.1)-(S2+0.02))/((N+0.1)+(S2+0.02)) # global vegetation moisture index
+#   ) |>
+#   dplyr::select(
+#     is_flue_drought,
+#     flue,
+#     GVMI,
+#     NMDI,
+#     evi,
+#     site
+#   )
 
-# plot flue data aligned by event (dday is the day into the event, inst is the event number)
-p <- ggplot(
-  data = flue_aligned$df_dday,
-  aes(x = dday, y = flue, group = inst, color = isevent)
-) +
-  geom_line() +
-  theme_classic() +
-  scale_color_manual(values = c("grey80", "tomato")
+df$is_flue_drought[is.na(df$is_flue_drought)] <- FALSE
+df <- df |>
+  select(
+    site,
+    is_flue_drought
   )
 
-print(p)
+bla <- df |>
+  group_by(site) |>
+  do({
+
+    x <- .
+    x$idx <- NA
+
+    # find breaks between TRUE 1 and FALSE 0
+    breaks <- diff(x$is_flue_drought)
+
+    # if not breaks return original
+    # else continue
+    if(!all(breaks == 0)){
+
+    # if start position is TRUE
+    # set start posiiton as 1
+    # else not
+    if(x$is_flue_drought[1]){
+      start <- c(1, which(breaks == 1))
+    } else {
+      start <- which(breaks == 1)
+    }
+
+    # which breaks transition from TRUE
+    # to FALSE and truncate on start
+    # length
+    end <- which(breaks == -1)
+    if(length(end) < length(start)){
+      end <- c(end, nrow(x))
+    }
+
+    # combine in one location matrix
+    # and exclude chunks which are shorter
+    # than 7 values (days)
+    locs <- cbind(start, end, end - start)
+    locs <- locs[locs[,3] > 7,1:2]
+    print(locs)
+
+    if (nrow(locs) > 0){
+      for (i in 1:nrow(locs)){
+        x$idx[locs[i,1]:locs[i,2]] <- i
+      }
+    }
+
+    } else {
+      x$idx <- NA
+    }
+
+    x
+
+  })
+
